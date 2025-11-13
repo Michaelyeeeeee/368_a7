dd52f0bc3fd0eb89ec486accddae644dee5849a3
michael <michaeldmwu@gmail.com>
2025-11-13 00:44:25 +0000

    r

--- a7.c
+++ a7.c
@@ -2,9 +2,10 @@
 
 int main(int argc, char **argv)
 {
+
     if (argc < 2)
         return EXIT_FAILURE;
-
+    fprintf(stdout, "check -\n");
     if (argv[1][0] != '-' || argv[1][1] == '\0')
         return EXIT_FAILURE;
 
